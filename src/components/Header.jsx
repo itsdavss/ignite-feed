792c72e83a94950cc57822047c5634957c631be4
Davi Oliveira <davi.ol.santos@gmail.com>
2025-02-21 08:10:38 -0300

    teste

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css'
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Ignite feed teste</strong>
+            <strong>Ignite feed</strong>
         </header>
     );
 }
