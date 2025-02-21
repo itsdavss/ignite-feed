20f3fce668aab8df907bd5ca6e47501d5f52b97c
Davi Oliveira <davi.ol.santos@gmail.com>
2025-02-21 09:11:22 -0300

    criando a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import './global.css'
 import { Header } from './components/Header'
+import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <>
-      <Header>
+      <Header />
 
-      </Header>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Davi Oliveira"
+            content="Lorem ipsum dolor sit amet"
+          />
+          <Post
+            author="Diego Fernandes"
+            content="Um novo post"
+          />
+        </main>
+      </div>
     </>
   )
 }
