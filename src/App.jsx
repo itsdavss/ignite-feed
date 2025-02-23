998650263bf11b8c16853fbbd8d639470fb85c8c
Davi Oliveira <davi.ol.santos@gmail.com>
2025-02-23 18:44:40 -0300

    adicionado o componente post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './global.css'
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
 import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
