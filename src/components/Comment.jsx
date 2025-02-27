178712df92aea8fe0f630cef1e896f3fb730d35a
Davi Oliveira <davi.ol.santos@gmail.com>
2025-02-27 08:21:07 -0300

    criando a função para deletar comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/itsdavss.png" />
@@ -15,7 +20,7 @@ export function Comment({ content }) {
               <time title='23 de fevereiro às 17:59h' datetime="2025-02-23 17:59:00">Cerca de 2h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
